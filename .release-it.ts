5d94803a5040fe4375fc3a75480d335df82e42ca
Gábor Torma <gabor@torma.dev>
2024-07-18 18:13:43 +0200

    fix: releaseName

--- .release-it.ts
+++ .release-it.ts
@@ -3,12 +3,13 @@ import { name } from './package.json'
 
 // eslint-disable-next-line no-template-curly-in-string
 const versionTemplate = '${version}'
+const releaseName = name.split('/').pop()
 
 export default {
   git: {
-    commitMessage: `Release: ${name} ${versionTemplate}`,
-    tagAnnotation: `Release: ${name} ${versionTemplate}`,
-    tagName: `${name}-v${versionTemplate}`,
+    commitMessage: `Release: ${releaseName} ${versionTemplate}`,
+    tagAnnotation: `Release: ${releaseName} ${versionTemplate}`,
+    tagName: `${releaseName}-v${versionTemplate}`,
     requireCommits: true,
     requireCommitsFail: false,
   },
