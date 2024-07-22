cf2dac5365ebe9c530bd6dc46e6126bc52b39ae7
Gábor Torma <gabor@torma.dev>
2024-07-22 16:18:09 +0200

    feat: use mwm for release

--- .release-it.ts
+++ .release-it.ts
@@ -1,22 +1,4 @@
-import type { Config } from 'release-it'
+import { defineReleaseItConfig } from '@gabortorma/mwm'
 import { name } from './package.json'
 
-// eslint-disable-next-line no-template-curly-in-string
-const versionTemplate = '${version}'
-const releaseName = name.split('/').pop()
-
-export default {
-  git: {
-    commitMessage: `Release: ${releaseName} ${versionTemplate}`,
-    tagAnnotation: `Release: ${releaseName} ${versionTemplate}`,
-    tagName: `${releaseName}-v${versionTemplate}`,
-    requireCommits: true,
-    requireCommitsFail: false,
-  },
-  github: {
-    release: true,
-  },
-  npm: {
-    publish: true,
-  },
-} satisfies Config
+export default defineReleaseItConfig('nuxt-layer', name.split('/').pop())
